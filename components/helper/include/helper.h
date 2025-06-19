b3be8caa23ca3c8b2eaff8165f019e7a81d20849
JanVych <132760222+JanVych@users.noreply.github.com>
2025-06-19 11:42:39 +0200

    add GetData (from broadcast frame)

--- components/helper/include/helper.h
+++ components/helper/include/helper.h
@@ -1,6 +1,13 @@
-#define ESP_RETURN_ERROR(x) do {    \
-    esp_err_t __err_rc = (x);       \
-    if (__err_rc != ESP_OK) {       \
-        return __err_rc;            \
-    }                               \
+#include "esp_err.h"
+
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#define ESP_RETURN_ERROR(x) do {      \
+    esp_err_t __err_rc = (x);         \
+    if (__err_rc != ESP_OK) {         \
+        return __err_rc;              \
+    }                                 \
 } while(0)
+
+#endif
--- main/main.c
+++ main/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "ecomax.h"
+#include "esp_log.h"
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    ecomax_data_t data = {0};
+    esp_err_t result;
+    ecomax_Init(UART_NUM_0, 17, 16);
+    while(true)
+    {
+        result = ecomax_GetData(&data);
+        if (result != ESP_OK)
+        {
+            ESP_LOGW("MAIN", "Failed to get data: %s", esp_err_to_name(result));
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI("MAIN", "Mix Temperature: %.2f", data.mixTemperature);
+        ESP_LOGI("MAIN", "Flue Temperature: %.2f", data.flueTemperature);
+        ESP_LOGI("MAIN", "TUV Temperature: %.2f", data.tuvTemperature);
+        ESP_LOGI("MAIN", "Boiler Temperature: %.2f", data.boilerTemperature);
+        ESP_LOGI("MAIN", "Acu Upper Temperature: %.2f", data.acuUpperTemperature);
+        ESP_LOGI("MAIN", "Acu Bottom Temperature: %.2f", data.acuBottomTemperature);
+        ESP_LOGI("MAIN", "Outside Temperature: %.2f", data.outsideTemperature);
+        ESP_LOGI("MAIN", "Oxygen Level: %.2f", data.oxygenLevel);
+
+        // ecomax_PrintFrame();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        ESP_LOGI("MAIN", "End");
+    }
 }
